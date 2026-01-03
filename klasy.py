a36d6bbb94575e27ecec3ca65cacf6708ef50e8e
Sylwia95 <sylwia407@onet.pl>
2026-01-03 11:25:47 +0000

    Chamges2

--- klasy.py
+++ klasy.py
@@ -2,21 +2,17 @@
 class Czlowiek:
     # Istota
     gatunek = "Homo Sapiens"
-    def __init__(self, imie):
-        # Konstruktor
-        # Akt Istnienia
-        print(f"Niech powstanie Czlowiek o imieniu {imie}")
+    def __init__ (self, imie):
+        print (f"Niech powstanie czlowiek o imieniu {imie}")
         self.imie = imie
-        # adam.imie = "Adam"
-        # ewa.imie = "Ewa"
 
-# Powstawanie obiektu
-# Gotowanie z przepisu
-adam = Czlowiek("Adam") # a = 4 # a = int(4)
-ewa = Czlowiek("Ewa")
-print(adam.gatunek)
-print(ewa.gatunek)
-print(adam.imie)
-print(ewa.imie)
+    def przedstaw_sie (self):
+        print ("Dzien dobry, mam na imie {self.imie}")
 
+    def przedstaw (self, osoba):
+        print (f"Oto {osoba.imie}")
+adam = Czlowiek ("Adam")
+ewa = Czlowiek ("Ewa")g
 
+ewa.przedstaw_sie()
+ewa.przedstaw(adam)
