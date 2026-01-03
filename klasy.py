dd7da004237497d136261621b94aec08fbecc765
Sylwia95 <sylwia407@onet.pl>
2026-01-03 12:37:57 +0000

    Changes3

--- klasy.py
+++ klasy.py
@@ -2,17 +2,40 @@
 class Czlowiek:
     # Istota
     gatunek = "Homo Sapiens"
-    def __init__ (self, imie):
+    def __init__ (self, imie, plec):
         print (f"Niech powstanie czlowiek o imieniu {imie}")
         self.imie = imie
+        self.plec = plec
 
     def przedstaw_sie (self):
-        print ("Dzien dobry, mam na imie {self.imie}")
+        print ("Dzien dobry, mam na imie {self.imie} i jestem ", end ="")
+        if self.plec == "M":
+            print ("mezczyzna")
+        else:
+            print ("kobieta")
 
     def przedstaw (self, osoba):
         print (f"Oto {osoba.imie}")
-adam = Czlowiek ("Adam")
-ewa = Czlowiek ("Ewa")g
+
+class Dziecko(Czlowiek):
+
+    def baw_sie(self):
+        print ("Ale zabawa, juhuu!!!!")
+    def przedstaw_sie (self):
+        print (f"Cesc, jestem {self.imie} i jestem ", end ="")
+        if self.plec == "M":
+            print ("chlopcem")
+        else:
+            print ("dziewczynka")
+
+
+
+adam = Czlowiek ("Adam", "M")
+ewa = Czlowiek ("Ewa", "K")
 
 ewa.przedstaw_sie()
 ewa.przedstaw(adam)
+kain = Dziecko("Kain", "M")
+kain.baw_sie()
+kain.przedstaw_sie()
+
