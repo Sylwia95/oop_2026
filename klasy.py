9de2294ae0753affdb1d4f9fb92f6912a3f8e8a0
Sylwia95 <sylwia407@onet.pl>
2026-01-03 10:27:09 +0000

    ^X

--- klasy.py
+++ klasy.py
@@ -1,5 +1,22 @@
+# Klasa = Szablon, Przepis
 class Czlowiek:
+    # Istota
     gatunek = "Homo Sapiens"
-    def __init__(self):
-        print ("Niech powstanie czlowiek")
+    def __init__(self, imie):
+        # Konstruktor
+        # Akt Istnienia
+        print(f"Niech powstanie Czlowiek o imieniu {imie}")
+        self.imie = imie
+        # adam.imie = "Adam"
+        # ewa.imie = "Ewa"
+
+# Powstawanie obiektu
+# Gotowanie z przepisu
+adam = Czlowiek("Adam") # a = 4 # a = int(4)
+ewa = Czlowiek("Ewa")
+print(adam.gatunek)
+print(ewa.gatunek)
+print(adam.imie)
+print(ewa.imie)
+
 
